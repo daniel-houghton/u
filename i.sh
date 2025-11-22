51d3d5b15a7c77bc97fba9ea1931bc1038423d0a
Daniel Houghton <dhoughton@middlebury.edu>
2025-11-22 07:57:02 -0500

    Update i.sh

--- i.sh
+++ i.sh
@@ -2,9 +2,9 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-sudo systemctl disable systemd-networkd-wait-online.service
-sudo apt update && sudo apt upgrade -y
-sudo apt install ubuntu-desktop -y
+# sudo systemctl disable systemd-networkd-wait-online.service
+# sudo apt update && sudo apt upgrade -y
+# sudo apt install ubuntu-desktop -y
 sudo apt update && apt install spice-vdagent spice-webdavd -y
 
 sudo apt update
